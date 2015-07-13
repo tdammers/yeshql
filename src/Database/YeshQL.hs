134e6a452c846326d9f40ee8f2074a7c5ab4e04f
Tobias Dammers <tdammers@gmail.com>
2015-07-13 10:48:57 +0200

    A primitive TH query maker

--- src/Database/YeshQL.hs
+++ src/Database/YeshQL.hs
@@ -1,2 +1,15 @@
+{-#LANGUAGE TemplateHaskell #-}
 module Database.YeshQL
 where
+
+import Language.Haskell.TH
+import Data.List (isPrefixOf)
+import Data.Maybe (catMaybes)
+
+mkQuery :: String -> Q Exp
+mkQuery q =
+    let items = words q
+        params = catMaybes . map (\x -> if (":" `isPrefixOf` x) then Just x else Nothing) $ items
+        items' = map (\x -> if (":" `isPrefixOf` x) then "?" else x) items
+        q' = unwords items'
+    in [|(q', params)|]

8e94f8450e8f499331af09409f8000a29682e212
Tobias Dammers <tdammers@gmail.com>
2017-09-07 21:53:38 +0200

    Removed syb-with-class dependency

--- src/Database/YeshQL/SqlRow/TH.hs
+++ src/Database/YeshQL/SqlRow/TH.hs
@@ -3,14 +3,16 @@
 {-#LANGUAGE TemplateHaskell #-}
 {-#LANGUAGE QuasiQuotes #-}
 {-#LANGUAGE LambdaCase #-}
+{-#LANGUAGE CPP #-}
 module Database.YeshQL.SqlRow.TH
+( makeSqlRow
+)
 where
 
 import Database.YeshQL.SqlRow.Class
 import Database.HDBC (fromSql, toSql)
 import Language.Haskell.TH
 import Language.Haskell.TH.Quote
-import Data.Generics.SYB.WithClass.Derive (typeInfo)
 
 makeSqlRow :: Name -> Q [Dec]
 makeSqlRow entityName = do
@@ -55,3 +57,36 @@ makeSqlRow entityName = do
             appE
                 (varE (mkName "fromSql"))
                 (varE (mkName $ "sql_" ++ nameBase fieldName))
+
+type Constructor =
+  (Name,         -- Name of the constructor
+   Int,          -- Number of constructor arguments
+   Maybe [Name], -- Name of the field selector, if any
+   [Type])       -- Type of the constructor argument
+
+typeInfo :: Dec
+         -> Q (Name,            -- Name of the datatype
+               [Name],          -- Names of the type parameters
+               [Constructor])   -- The constructors
+typeInfo d
+        = case d of
+#if MIN_VERSION_template_haskell(2,11,0)
+            DataD    _ n ps _ cs _ -> return (n, map varName ps, map conA cs)
+            NewtypeD _ n ps _ c  _ -> return (n, map varName ps, [conA c])
+#else
+            DataD    _ n ps cs _ -> return (n, map varName ps, map conA cs)
+            NewtypeD _ n ps c  _ -> return (n, map varName ps, [conA c])
+#endif
+            _ -> error ("derive: not a data type declaration: " ++ show d)
+        where
+            conA (NormalC c xs) = (c, length xs, Nothing, map snd xs)
+            conA (InfixC x1 c x2) = conA (NormalC c [x1, x2])
+            conA (ForallC _ _ c) = conA c
+            conA (RecC c xs) =
+                    let getField (n, _, _) = n
+                        getType  (_, _, t) = t
+                        fields = map getField xs
+                        types  = map getType xs
+                    in (c, length xs, Just fields, types)
+            varName (PlainTV n) = n
+            varName (KindedTV n _) = n

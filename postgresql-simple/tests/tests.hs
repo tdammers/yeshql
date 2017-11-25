ff2f37ec08fdac8eab2a6d14a1a87818f019dd0e
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2017-11-25 19:41:19 +0100

    Read the PostgreSQL connection string from the YESHQL_POSTGRESQL_CONNSTRING envvar

--- postgresql-simple/tests/tests.hs
+++ postgresql-simple/tests/tests.hs
@@ -2,18 +2,29 @@
 module Main where
 
 import Control.Exception
-import Test.Tasty
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as ByteString
 import Database.PostgreSQL.Simple
 import qualified Database.YeshQL.PostgreSQL.Tests as Tests
+import System.Environment
+import System.Exit
+import System.IO
+import Test.Tasty
 
 main :: IO ()
-main = withConn (defaultMain . allTests)
+main = do
+  mConnString <- lookupEnv "YESHQL_POSTGRESQL_CONNSTRING"
+  case mConnString of
+    Nothing -> do
+      hPutStrLn stderr "Please set the 'YESHQL_POSTGRESQL_CONNSTRING' to the PostgreSQL connection string"
+      exitFailure
+    Just connString -> withConn (ByteString.pack connString) (defaultMain . allTests)
   where
     allTests conn = testGroup "All Tests" (Tests.tests conn)
 
-withConn :: (Connection -> IO a) -> IO a
-withConn f =
-  bracket (connectPostgreSQL "host='localhost' user='postgres'") close $ \c ->
+withConn :: ByteString -> (Connection -> IO a) -> IO a
+withConn connString f =
+  bracket (connectPostgreSQL connString) close $ \c ->
   bracket_ (begin c) (rollback c) $ do
     initializeDb c
     f c
